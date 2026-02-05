68d6b936495fa910d8acf944b8cf3eaaee9571aa
Simler Pascal <pascal.simler@ch.gudel.com>
2026-02-05 14:00:03 +0100

    Added support for multiple database providers

---
+++ Code/Tests/ConsoleTests/Program.cs
@@ -0,0 +1,42 @@
+﻿using Gudel.GLogWare.EFCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddGLogWareDbContext("Host=localhost;Port=5432;Database=GLogWare_CompanyCity;Username=admin;Password=*Gudel1954*");
+    })
+    .Build();
+
+// Resolve and use DbContext
+using var scope = host.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<GLogWareDbContext>();
+
+Console.WriteLine("DbContext successfully created");
+
+//foreach (var inv in db.VInventories)
+//{
+//    Console.WriteLine($"{inv.Place} - {inv.Amount}");
+//}
+
+Console.WriteLine("----------------------");
+
+foreach (var a in db.Areas)
+{
+    Console.WriteLine(a.Name);
+}
+
+//Place pl = new Place();
+//pl.Name = "KOM-1-1";
+//pl.AreaName = "KOM";
+//db.Places.Add(pl);
+
+var komArea = db.Areas.Where(a => a.Name == "GANTRY").FirstOrDefault();
+if (komArea != null)
+{
+    komArea.Comments = DateTime.Now.ToString("HH:mm:ss");
+    komArea.LastUpdatedBy = "Cruchot";
+}
+
+db.SaveChanges();
